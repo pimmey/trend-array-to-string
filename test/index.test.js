59dc5e9a1deb6d3f57269267ec975a8fea1a8cb9
Yegor Borisenco <pimmey@pimmey.com>
2017-05-04 09:54:49 +0300

    Added some npm commands and tests

---
+++ test/index.test.js
@@ -0,0 +1,59 @@
+import chai from 'chai';
+import arrayToString from '../src';
+const expect = chai.expect;
+
+describe('Trendy array to fancy string', () => {
+  const tests = [
+    {
+      input: [1, 2, 3, 4, 5, 6, 7, 8],
+      output: '1-8'
+    },
+    {
+      input: [1, 3, 4, 5, 6, 7, 8],
+      output: '1,3-8'
+    },
+    {
+      input: [1, 3, 4, 5, 6, 7, 8, 10, 11, 12],
+      output: '1,3-8,10-12'
+    },
+    {
+      input: [1, 2, 3],
+      output: '1-3'
+    },
+    {
+      input: [1, 2],
+      output: '1,2'
+    },
+    {
+      input: [1, 2, 4],
+      output: '1,2,4'
+    },
+    {
+      input: [1, 2, 4, 5, 6],
+      output: '1,2,4-6'
+    },
+    {
+      input: [1, 2, 3, 7, 8, 9, 15, 17, 19, 20, 21],
+      output: '1-3,7-9,15,17,19-21'
+    },
+    {
+      input: [1, 2, 3, 4, 5, 6, 100, 1091, 1999, 2000, 2001, 2002],
+      output: '1-6,100,1091,1999-2002'
+    },
+    {
+      input: [1],
+      output: '1'
+    },
+    {
+      input: [1, 3, 5, 7, 9, 11],
+      output: '1,3,5,7,9,11'
+    }
+  ];
+
+  tests.map(test => {
+    it(`returns ${test.output} when passing [${test.input}]`, done => {
+      expect(arrayToString(test.input)).to.equal(test.output);
+      done();
+    })
+  });
+});
